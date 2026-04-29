a7c6c68a1d7a05b237788a626f98a496e0073def
aser9988q-web <aser9988q@gmail.com>
2026-04-29 13:46:48 +0000

    Add files via upload

--- server.js
+++ server.js
@@ -10,12 +10,25 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.static(join(__dirname)));
 
-app.get('/', (req, res) => {
+// تقديم الملفات الثابتة من المجلد الحالي
+app.use(express.static(__dirname, {
+  maxAge: '1d',
+  etag: false
+}));
+
+// تقديم مجلد assets بشكل صريح
+app.use('/assets', express.static(join(__dirname, 'assets'), {
+  maxAge: '1d',
+  etag: false
+}));
+
+// جميع الطلبات الأخرى ترجع index.html (للتطبيقات أحادية الصفحة)
+app.get('*', (req, res) => {
   res.sendFile(join(__dirname, 'index.html'));
 });
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+  console.log(`Serving static files from ${__dirname}`);
 });
